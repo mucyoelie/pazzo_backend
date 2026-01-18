5f0ad65362d9a775571bd955e83cb43c13f2e867
Mucyo Elie Joseph <147175412+mucyoelie@users.noreply.github.com>
2026-01-18 23:22:33 +0200

    update ccs schema

--- schema/ccs.js
+++ schema/ccs.js
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
 
 const ccsSchema = new mongoose.Schema({
-  name: String,
-  price: Number,
-  description: String,
-  image: String
-});
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+  description: { type: String, required: true },
+  image: { type: Buffer },      // raw image stored as binary
+  imageType: { type: String },  // MIME type (e.g. image/png)
+}, { timestamps: true });
 
 const CCS = mongoose.model("CCS", ccsSchema);
 export default CCS;
+;
 
